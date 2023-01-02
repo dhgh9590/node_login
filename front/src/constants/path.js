9afcf9f84879e09ba0b27209104ca230de078484
오호설 <79558348+dhgh9590@users.noreply.github.com>
2023-01-02 13:13:13 +0900

    Delete front directory

--- front/src/constants/path.js
+++
@@ -1,9 +0,0 @@
-/* Router의 path 관리 */
-
-export const BASE_PATH = '/calendar';
-
-export const PATH = {
-  MAIN: '/',
-  DAY: '/day',
-  WEEK: '/week',
-};
--- front/src/pages/Main/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-// import axios from 'axios';
-
-export const Main = () => {
-  return <div>메인</div>;
-};
-
-export default Main;
